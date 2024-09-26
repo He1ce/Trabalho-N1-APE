b8c4adff038d94e6328cfdf66e790c43585f7ce2
Thiago Vieira <thiago.skrull@gmail.com>
2024-09-25 21:12:49 -0300

    Versão 1.3
    Atualizada a main.c pra o recebimento de mensagens em hexadecimal

--- main.c
+++ main.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
-#include "func_val.h"
+#include "val.h"
 #include "decodificador.h"
 
 int main()
 {
-    int pos, qtd_mensagens, b;
-    char teste[] = "566F6388732073C66F2076656E6365646F867265732C20766F63C3887320636FBE6E73656775656D2E002DC6C921B7B87FCF";
+    int qtd_mensagens, b;
+    char teste[101];
 
     printf("Insira a quantidade de mensagens interceptadas: ");
     scanf("%d", &qtd_mensagens);
 
     for(int i = 0; i < qtd_mensagens;i++)
     {
-        printf("Digite a posição do caracter: ");
-        scanf("%d", &pos);
+        
         printf("Insira o valor de B: ");
         scanf("%d", &b);
+
+        printf("Insira a mensagem: ");
+        fgets(teste, 101, stdin);
+
+        decodificador(teste, b);
     }
 
-    printf("Valor func_val: %d\n", func_val(pos,b));
 
-    decodificador(teste, b);
-
+    
     return 0;
 }
+
