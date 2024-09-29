d1e706a08fb3f4f36727f30d700872c8bf7a6a35
Thiago Vieira <thiago.skrull@gmail.com>
2024-09-29 14:59:36 -0300

    Adição de comentários no código decodificador.h
    Adição de comentários no código para explicar como funciona.

--- Decodificador/decodificador.h
+++ Decodificador/decodificador.h
@@ -1,6 +1,6 @@
 #ifndef DECODIFICADOR_H
 #define DECODIFICADOR_H
 
-void decodificador(char codigo[101], int b);
+void decodificador(char codigo[101], int b);// Declaração da função decodificador
 
 #endif
