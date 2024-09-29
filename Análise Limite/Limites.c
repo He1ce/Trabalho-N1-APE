8796983492668b4c9d7fb1c246cb73c75ddf963d
Arthur Vieira <ArthurVsousa.ec@outlook.com>
2024-09-29 10:33:31 -0300

    Organização em pasta e renomeação
    Update e troca de nome de limites variáveis.c para Análise Limite/Limites.c

--- Análise Limite/Limites.c
+++ Análise Limite/Limites.c
@@ -4,10 +4,10 @@
 
 int main() {
 	setlocale(LC_ALL, "Portuguese");
-	printf("1) Tabela de limites m·ximos e mÌnimos das vari·veis:\n");
+	printf("1) Tabela de limites m√°ximos e m√≠nimos das vari√°veis:\n");
 	printf("\n");
 	printf("----------------------------------------------------------------------------------------------------------\n");
-	printf("        Vari·vel        |               M·ximo            |                         MÌnimo                \n");
+	printf("        Vari√°vel        |               M√°ximo            |                         M√≠nimo                \n");
 	printf("----------------------------------------------------------------------------------------------------------\n");
 	printf("          char                     %d                                       %d                            \n", CHAR_MAX, CHAR_MIN);
 	printf("           int                     %d                                       %d                            \n", INT_MAX, INT_MIN);
@@ -20,21 +20,21 @@ int main() {
 	printf("----------------------------------------------------------------------------------------------------------\n");
  
     printf("\n");
- 	printf("2) RepresentaÁ„o e explicaÁ„o do que acontece ao adcionar um valor maior do que o suportado pela vari·vel: \n");
+ 	printf("2) Representa√ß√£o e explica√ß√£o do que acontece ao adcionar um valor maior do que o suportado pela vari√°vel: \n");
     char a;  
 
     a = 300;  
 
-    printf("Valor atribuÌdo a 'a': %d\n", a);
+    printf("Valor atribu√≠do a 'a': %d\n", a);
     printf("Valor de 'a' como char: %c\n", a);
-    printf("ExplicaÁ„o: ao atribuir um n˙mero maior do que o suportado pela vari·vel char, no intervalo ela vai ir diminuindo o  valor indicado por 256 que È o m·ximo, apresentado assim, resultados da tabela ASCII na execuÁ„o.\n ");
+    printf("Explica√ß√£o: ao atribuir um n√∫mero maior do que o suportado pela vari√°vel char, no intervalo ela vai ir diminuindo o  valor indicado por 256 que √© o m√°ximo, apresentado assim, resultados da tabela ASCII na execu√ß√£o.\n ");
 
     printf("\n");
-    printf("3)Conceito das vari·veis:\n");
-    printf("a. char: armazena letras, n˙meros e sÌmbolos.\n");
-    printf("b. int: usado principalmente em operaÁıes aritmÈticas inteiras e contagens.\n");
-    printf("c. short int: usado para economizar memÛria e os valores inteiros s„o pequenos.\n");
-    printf("d. unsigned int: usado em contagens e manipulaÁıes de Ìndices, operaÁıes que n„o se usa n˙meros negagtivos.\n ");
+    printf("3)Conceito das vari√°veis:\n");
+    printf("a. char: armazena letras, n√∫meros e s√≠mbolos.\n");
+    printf("b. int: usado principalmente em opera√ß√µes aritm√©ticas inteiras e contagens.\n");
+    printf("c. short int: usado para economizar mem√≥ria e os valores inteiros s√£o pequenos.\n");
+    printf("d. unsigned int: usado em contagens e manipula√ß√µes de √≠ndices, opera√ß√µes que n√£o se usa n√∫meros negagtivos.\n ");
     printf("e. long int: usado para contagens grandes, mais complexas que utilizam intervalos.\n");
     printf("f. unsigned long int: quando necessita de um intervalo maior sem permitir valores negativos.\n");
     printf("g. long long int: para valores inteiros que podem exceder o intervalo de um long.\n");
